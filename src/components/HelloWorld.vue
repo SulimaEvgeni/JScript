2555710221979c610fbb4c1bcd1f3e88f7dd0818
SulimaEvgeni <sulima123@gmail.com>
2021-01-11 11:19:36 +0200

    update my project

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>{{ now }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  },
+	data() {
+		return {
+			now:  this.getNow()
+		};
+	},
+  created() {
+    setInterval(() => {
+      this.getNow();
+    }, 1000)
+  },
+  methods: {
+    getNow: function() {
+      const today = new Date();
+      const date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+      const time = ("0" + today.getHours()).slice(-2) + ":" + ("0" + today.getMinutes()).slice(-2) + ":" +  ("0" + today.getSeconds()).slice(-2) ;
+      const dateTime = date + ' ' + time;
+      this.now = dateTime;
+    }
+  }
+};
+</script>
